b703c59fe90d3c02171d7139c262a8619c4ebb3b
Isaiah Flager-Hearan <94151429+PapaZay@users.noreply.github.com>
2026-03-13 17:34:59 -0500

    adding initial spring boot files

---
+++ src/main/java/com/edgarfeed/EdgarfeedApplication.java
@@ -0,0 +1,13 @@
+package com.edgarfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgarfeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgarfeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edgarfeed/EdgarfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgarfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgarfeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
