a82f5cdc43940415b7be2dca364a5670eec08225
Isaiah Flager-Hearan <94151429+PapaZay@users.noreply.github.com>
2026-03-15 17:26:34 -0500

    added Price class

---
+++ src/main/java/com/edgarfeed/model/Price.java
@@ -0,0 +1,27 @@
+package com.edgarfeed.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "prices")
+public class Price {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String ticker;
+    private LocalDate date;
+    private BigDecimal open;
+    private BigDecimal high;
+    private BigDecimal low;
+    private BigDecimal close;
+    private BigInteger volume;
+
+}
