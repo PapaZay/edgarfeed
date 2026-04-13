b2bfe3ec66cb07603052072acf947c0c632bc1b5
Isaiah Flager-Hearan <94151429+PapaZay@users.noreply.github.com>
2026-04-12 20:47:15 -0500

    added TradeService functions

---
+++ frontend/src/api/TradeService.ts
@@ -0,0 +1,24 @@
+import type {Trade, Page} from "./types";
+
+const BASE = '/api'
+
+export async function getTrades(page = 0): Promise<Page<Trade>> {
+    const res = await fetch(`${BASE}/trades?page=${page}&sort-filedDate,desc`)
+    return res.json()
+}
+
+export async function getTradesById(id: number): Promise<Trade> {
+    const res = await fetch(`${BASE}/trades/${id}`)
+    return res.json()
+}
+
+export async function getTradesByTicker(ticker: string): Promise<Trade[] {
+    const res = await fetch(`${BASE}/tickers/${ticker}/trades`)
+    return res.json()
+}
+
+export async function getTradesByInsider(name: string): Promise<Trade[]> {
+    const res = await fetch(`${BASE}/insiders/${encodeURIComponent(name)}/trades`)
+    return res.json()
+}
+
